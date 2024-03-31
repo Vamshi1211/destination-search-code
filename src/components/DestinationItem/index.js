f8f604d2660a69d3d690d1ba86db33323eda7e22
vamshi <vamshigundepangu932@gmail.com>
2024-03-31 10:51:18 +0000

    Destination Search Code With Solution

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -1,16 +1,15 @@
-// Write your code here
 import './index.css'
 
-const DestinationItems = props => {
-  const {eachDestinationItem} = props
-  const {name, imgUrl, key} = eachDestinationItem
-  //   console.log(name)
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {imgUrl, name} = destinationDetails
+
   return (
-    <li className="destination-item-container">
-      <img src={imgUrl} alt={name} className="destination-img" />
-      <p className="destination-item">{name}</p>
+    <li className="destination-item">
+      <img src={imgUrl} alt={name} className="destination-image" />
+      <p className="name">{name}</p>
     </li>
   )
 }
 
-export default DestinationItems
+export default DestinationItem
--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,48 +1,52 @@
-// Write your code here
 import {Component} from 'react'
-import DestinationItems from '../DestinationItem'
+
+import DestinationItem from '../DestinationItem'
+
 import './index.css'
 
 class DestinationSearch extends Component {
-  state = {searchInput: ''}
+  state = {
+    searchInput: '',
+  }
 
-  onChangeSearchResults = event => {
+  onChangeSearchInput = event => {
     this.setState({searchInput: event.target.value})
   }
 
   render() {
-    const {destinationsList} = this.props
     const {searchInput} = this.state
-
-    const searchResults = destinationsList.filter(eachItem =>
-      eachItem.name.toLowerCase().includes(searchInput),
+    const {destinationsList} = this.props
+    const searchResults = destinationsList.filter(eachDestination =>
+      eachDestination.name.toLowerCase().includes(searchInput.toLowerCase()),
     )
-    // const getDestinationList = this.getDestination(destinationsList)
+
     return (
-      <div className="bg-container">
-        <h1 className="destination-search">Destination Search</h1>
-        <div className="search-container">
-          <input
-            placeholder="Search"
-            type="search"
-            className="input-element"
-            onChange={this.onChangeSearchResults}
-            value={searchInput}
-          />
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-            alt="search icon"
-            className="image"
-          />
-        </div>
-        <ul className="destination-items-container">
-          {searchResults.map(eachItem => (
-            <DestinationItems
-              eachDestinationItem={eachItem}
-              key={eachItem.id}
+      <div className="app-container">
+        <div className="destinations-search-container">
+          <h1 className="heading">Destination Search</h1>
+          <div className="search-input-container">
+            <input
+              type="search"
+              className="search-input"
+              placeholder="Search"
+              value={searchInput}
+              onChange={this.onChangeSearchInput}
             />
-          ))}
-        </ul>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+              alt="search icon"
+              className="search-icon"
+            />
+          </div>
+          <ul className="destinations-list">
+            {searchResults.map(eachDestination => (
+              <DestinationItem
+                key={eachDestination.id}
+                destinationDetails={eachDestination}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     )
   }
